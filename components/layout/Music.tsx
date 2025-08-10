806bcfe30831fccd59b38a0c9aeadee7c0e941a5
DabAZ <dabaz@dabaz.me>
2025-08-11 00:43:49 +0800

    Update Music.tsx

--- components/layout/Music.tsx
+++ components/layout/Music.tsx
@@ -12,7 +12,7 @@ const Music = () => {
             I dive into the rhythm of the streets, crafting beats and rhymes with raw energy, using tools like <Link href="https://www.image-line.com" target="_blank" className="underline bg-black text-[#bee54b]">FL Studio</Link> & <Link href="https://www.apple.com/logic-pro" target="_blank" className="underline bg-black text-[#bee54b]">Logic Pro</Link> to shape the soundscape of the urban night.
           </p>
           <p className="mt-8 text-md bg-white sm:text-lg/8 font-mono">
-            Since 2017, I've been spitting bars & notes that echo the grit of the city and the heart of my story—tracks like Athanasia hit deep, a tribute to my mom. From freestyles like Skrt Freestyle to soulful anthems, I blend hip-hop's pulse with personal truths, turning late-night vibes into sonic hacks that resonate. I'm always chasing the next beat to make your soul move. Check out my music to learn more (click "Enter").
+            Since 2017, I&apos;ve been spitting bars & notes that echo the grit of the city and the heart of my story—tracks like Athanasia hit deep, a tribute to my mom. From freestyles like Skrt Freestyle to soulful anthems, I blend hip-hop&apos;s pulse with personal truths, turning late-night vibes into sonic hacks that resonate. I&apos;m always chasing the next beat to make your soul move. Check out my music to learn more (click "Enter").
           </p>
         </div>
         <div className="flex justify-end -mt-4">

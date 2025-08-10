0ed2c287f84a7d19611d5ace01382050fb5fe9f2
DabAZ <dabaz@dabaz.me>
2025-08-11 00:24:46 +0800

    Add hero section

---
+++ components/layout/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div className="relative font-sans overflow-hidden bg-[url(/images/lightning.png)] bg-center bg-no-repeat min-h-screen">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8 py-32 sm:py-48 lg:py-56">
+        <p className="text-9xl sm:text-[12em] text-center">
+          DabAZ
+        </p>
+        <p className="mt-16 sm:mt-8 text-7xl sm:text-[6em] text-center">
+          hacker. musician. developer. designer.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
